a7a1e1012bc8c83badd7fa9fc300e3afb482b1f9
Gustavo Santos <gustavo@send4.com.br>
2020-01-15 21:34:47 -0300

    start project

---
+++ src/promises/types.ts
@@ -0,0 +1,8 @@
+export type FunctionA1<A, B> = (arg: A) => B;
+export type FunctionA2<A, B, C> = (arg0: A, arg1: B) => C;
+export type FunctionA3<A, B, C, D> = (arg0: A, arg1: B, arg2: C) => D;
+export type FunctionA4<A, B, C, D, E> = (arg0: A, arg1: B, arg2: C, arg3: D) => E;
+
+export type FunctionPromiseA1<A, B> = (arg: A) => Promise<B>;
+export type FunctionPromiseA2<A, B, C> = (arg0: A, arg1: B) => Promise<C>;
+export type FunctionPromisePromiseA1<A, B> = (arg: Promise<A>) => Promise<B>;
